45c431a505f2fcc4025dcf3f8a07aca72148a3e8
feindreh <119032218+feindreh@users.noreply.github.com>
2023-06-21 23:00:02 +0200

    moved sudokuSolver logic to own file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./app.css"
 import Sudoku from "./sudoku";
 import Buttons from "./buttons";
 import { useState } from "react";
-
+import SudokuSolver from "./sudokuSolver";
 
 const makeEmptySudoku = () => {
   const arr = []
@@ -55,66 +55,7 @@ function App() {
     setSolving(false)
     setSudoku([...sudoku])
   }
-  async function SudokuSolver(grid,refresh){
-    const validMap = (map) => {
-      for (const key in map) {
-        if (map[key] > 1) { return false; }
-      }
-      return true;
-    };
-    function isValid(x, y) {
-      // check x
-      const xmap = {};
-      for (let i = 0; i < 9; i++) {
-        if (grid[i][y] === '') { continue; }
-        xmap[grid[i][y]] ? xmap[grid[i][y]]++ : xmap[grid[i][y]] = 1;
-      }
-      if (validMap(xmap) === false) { return false; }
   
-      // check y
-  
-      const ymap = {};
-      for (let i = 0; i < 9; i++) {
-        if (grid[x][i] === '') { continue; }
-        ymap[grid[x][i]] ? ymap[grid[x][i]]++ : ymap[grid[x][i]] = 1;
-      }
-      if (validMap(ymap) === false) { return false; }
-  
-      // check box
-  
-      const xStart = Math.floor(x / 3) * 3;
-      const yStart = Math.floor(y / 3) * 3;
-      const bmap = {};
-      for (let i = xStart; i < xStart + 3; i++) {
-        for (let j = yStart; j < yStart + 3; j++) {
-          if (grid[i][j] === '') { continue; }
-          bmap[grid[i][j]] ? bmap[grid[i][j]]++ : bmap[grid[i][j]] = 1;
-        }
-      }
-      if (validMap(bmap) === false) { return false; }
-  
-      return true;
-    }
-    const bt = async(x, y) => {
-      if (y >= 9) { return true; }
-      if (x >= grid.length) { return  await bt(0, y + 1); }
-      if (grid[x][y] !== '') { return  await bt(x + 1, y); }
-      for (let i = 1; i <= 9; i++) {
-        function timeout(ms) {
-          return new Promise(resolve => setTimeout(resolve, ms));
-        }
-        grid[x][y] = `${i}`;
-        refresh()
-        await timeout(0)
-        if (isValid(x, y) === false) { continue; }
-        
-        if ( await bt(x + 1, y)) { return true; }
-      }
-      grid[x][y] = '';
-      return false;
-    };
-    await bt(0, 0);
-  };
 
 
   return (
--- src/sudokuSolver.js
+++ src/sudokuSolver.js
@@ -1,45 +1,62 @@
-const isValidSudoku = (board) => {
-  const isValidBlock = (arr) => {
-    const count = {};
-    arr.forEach((number) => {
-      count[number] ? count[number]++ : count[number] = 1;
-    });
-    for (const key in count) {
-      if (count[key] > 1 && key !== '') {
-        return false;
-      }
+async function SudokuSolver(grid,refresh){
+  const validMap = (map) => {
+    for (const key in map) {
+      if (map[key] > 1) { return false; }
     }
     return true;
   };
-  for (let i = 0; i < 9; i++) {
-    // check rows
-    if (isValidBlock(board[i]) === false) {
-      return false;
+  function isValid(x, y) {
+    // check x
+    const xmap = {};
+    for (let i = 0; i < 9; i++) {
+      if (grid[i][y] === '') { continue; }
+      xmap[grid[i][y]] ? xmap[grid[i][y]]++ : xmap[grid[i][y]] = 1;
     }
-    // check columns
-    const column = [];
-    for (let j = 0; j < 9; j++) {
-      column.push(board[j][i]);
+    if (validMap(xmap) === false) { return false; }
+
+    // check y
+
+    const ymap = {};
+    for (let i = 0; i < 9; i++) {
+      if (grid[x][i] === '') { continue; }
+      ymap[grid[x][i]] ? ymap[grid[x][i]]++ : ymap[grid[x][i]] = 1;
     }
-    if (isValidBlock(column) === false) {
-      return false;
-    }
-  }
-  // check 3x3
-  for (let x = 0; x < 9; x += 3) {
-    for (let y = 0; y < 9; y += 3) {
-      const block = [];
-      for (let i = x; i < x + 3; i++) {
-        for (let j = y; j < y + 3; j++) {
-          block.push(board[i][j]);
-        }
+    if (validMap(ymap) === false) { return false; }
+
+    // check box
+
+    const xStart = Math.floor(x / 3) * 3;
+    const yStart = Math.floor(y / 3) * 3;
+    const bmap = {};
+    for (let i = xStart; i < xStart + 3; i++) {
+      for (let j = yStart; j < yStart + 3; j++) {
+        if (grid[i][j] === '') { continue; }
+        bmap[grid[i][j]] ? bmap[grid[i][j]]++ : bmap[grid[i][j]] = 1;
       }
-      if (isValidBlock(block) === false) {
-        return false;
-      }
     }
+    if (validMap(bmap) === false) { return false; }
+
+    return true;
   }
-  return true;
+  const bt = async(x, y) => {
+    if (y >= 9) { return true; }
+    if (x >= grid.length) { return  await bt(0, y + 1); }
+    if (grid[x][y] !== '') { return  await bt(x + 1, y); }
+    for (let i = 1; i <= 9; i++) {
+      function timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      grid[x][y] = `${i}`;
+      refresh()
+      await timeout(0)
+      if (isValid(x, y) === false) { continue; }
+      
+      if ( await bt(x + 1, y)) { return true; }
+    }
+    grid[x][y] = '';
+    return false;
+  };
+  await bt(0, 0);
 };
 
-export default isValidSudoku;
+export default SudokuSolver
