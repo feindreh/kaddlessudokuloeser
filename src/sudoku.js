605e8be20c7c45fd1c27f0e38bd3767e6fa780d8
feindreh <119032218+feindreh@users.noreply.github.com>
2023-06-21 22:35:44 +0200

    add className toggle with prop solving

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,13 +1,14 @@
 import SudokuSquare from "./sudokuSquare";
 
-
 function Sudoku(props) {
-    const {sudoku,update} = props
-    return (<div id="sudoku">
-        {sudoku.map((row,x) => {
-            return row.map((n,y) => <SudokuSquare value = {n}update={update}key={""+x+y}x={x} y={y}/>)
-        })}
-    </div>);
+    const {solving,sudoku,update} = props
+    return (
+        <div id="sudoku" className = {solving?"unClickAble":null}>
+            {sudoku.map((row,x) => {
+                return row.map((n,y) => <SudokuSquare value = {n}update={update}key={""+x+y}x={x} y={y}/>)
+            })}
+        </div>
+    );
 }
 
 export default Sudoku;
